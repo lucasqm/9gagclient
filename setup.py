691ed8647e32fee86e78d8e5e9c07aadc4e042be
Lucas Quintela <lucas.qmiranda@gmail.com>
2019-07-10 13:27:44 -0300

    fix dependencies

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import re
 import os
 from setuptools import find_packages, setup
 
-DEPENDENCIES = ['requests', 're', 'logging']
+DEPENDENCIES = ['requests']
 EXCLUDE_FROM_PACKAGES = ["contrib", "docs", "tests*"]
 CURDIR = os.path.abspath(os.path.dirname(__file__))
 
